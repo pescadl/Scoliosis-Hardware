52b4bf5a937134ddc8378ff88a6d1fe25999f203
muddpooja97 <muddpooja97@gmail.com>
2019-06-07 18:42:21 -0500

    restructuring

--- ble5_project_zero_cc2640r2lp_app/FlashROM_StackLibrary/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/ti/simplelink_cc2640r2_sdk_2_40_00_32/source/ti/devices/cc26x0r2/driverlib/bin/ccs/driverlib.lib" -l"C:/ti/simplelink_cc2640r2_sdk_2_40_00_32/kernel/tirtos/packages/ti/dpl/lib/dpl_cc26x0r2.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_2_40_00_32/source/ti/drivers/lib/drivers_cc26x0r2.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_2_40_00_32/source/ti/drivers/rf/lib/rf_singleMode_cc26x0r2.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_2_40_00_32/source/ti/display/lib/display.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_2_40_00_32/source/ti/grlib/lib/ccs/m3/grlib.a" -l"C:/Users/dayoung/school/ee364d/workspace_v8/ble5_project_zero_cc2640r2lp_stack_library/FlashROM_Library/ble_r2.symbols" -l"C:/Users/dayoung/school/ee364d/workspace_v8/ble5_project_zero_cc2640r2lp_stack_library/FlashROM_Library/lib_linker.cmd" -l"C:/Users/dayoung/school/ee364d/workspace_v8/ble5_project_zero_cc2640r2lp_stack_library/FlashROM_Library/ble5_project_zero_cc2640r2lp_stack_library.lib" -l"C:/ti/simplelink_cc2640r2_sdk_2_40_00_32/source/ti/ble5stack/common/cc26xx/ccs/cc26xx_app.cmd" -llibc.a
-
